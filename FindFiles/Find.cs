8f368bfda5c2c154797873ae8b4f2250b8a859fb
Flavio <flaviocampelo@hotmail.com>
2022-03-16 22:43:55 -0400

    Initial commit

---
+++ FindFiles/Find.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace FindFiles
+{
+    public class Find
+    {
+        [Benchmark]
+        public void RepositoryOnly()
+        {
+            Directory.GetFiles(@"C:\FindFiles\RepositoryOnly", "*.xml", SearchOption.TopDirectoryOnly);
+        }
+
+        [Benchmark]
+        public void SubRepository()
+        {
+            Directory.GetFiles(@"C:\FindFiles\SubRepository", "*.xml", SearchOption.AllDirectories);
+        }
+    }
+}
---
+++ FindFiles/Program.cs
@@ -0,0 +1,4 @@
+﻿using BenchmarkDotNet.Running;
+using FindFiles;
+
+var summary = BenchmarkRunner.Run<Find>();
